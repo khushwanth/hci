2a19ac26a309d50650ed2397088112e809d67064
khushwanth <khushwanth.chhetri@gmail.com>
2018-04-10 20:58:59 +0600

    Initial Project Upload

---
+++ app/src/main/java/bt/edu/cst/www/hcidesign/Maintenance.java
@@ -0,0 +1,13 @@
+package bt.edu.cst.www.hcidesign;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Maintenance extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maintenance);
+    }
+}
